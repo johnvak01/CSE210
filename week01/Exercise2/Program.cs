b09fd4adb1b48f54c92b4ad02e4ac018da1c024c
johnvak01 <johnvak01@gmail.com>
2025-10-26 19:35:21 -0600

    Completed assignment 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // get input
+        Console.Write("What is your grade(as a percentage)? ");
+        int gradePercent = int.Parse(Console.ReadLine());
+        // calculate grade letter
+        string gradeLetter = "";
+        if(gradePercent>=90){
+            gradeLetter = "A";
+        } 
+        else if(gradePercent >=80){
+            gradeLetter = "B";
+
+        }
+        else if(gradePercent >=70){
+            gradeLetter = "C";
+
+        }
+        else if(gradePercent >=60){
+            gradeLetter = "D";
+
+        }
+        else{
+            gradeLetter = "F";
+        }
+        Console.Write($"Your Grade is: {gradeLetter}\n");
+
+        // calculate pass
+        if(gradeLetter!="F"){
+            Console.Write("Congratulations! You pass!");
+        }else{
+            Console.Write("You Fail! Better luck next time!");
+        }
     }
 }
