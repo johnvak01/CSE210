9fda87724555ce7eee962043550ba018a3bd8d09
johnvak01 <johnvak01@gmail.com>
2025-10-27 19:52:42 -0600

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random generator = new Random();
+        int magic_number = generator.Next(1,100);
+        while(true){
+            Console.Write("What is the your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            if(guess < magic_number){
+                Console.WriteLine("Higher");
+            }
+            else if(guess > magic_number){
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+        }
     }
 }
