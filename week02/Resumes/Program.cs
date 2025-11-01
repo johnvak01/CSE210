0be823f589bd38741dbfbd824d15005d8b5ca066
johnvak01 <johnvak01@gmail.com>
2025-11-01 07:56:21 -0600

    completed week 2 assignment 1

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
+    Job job1 = new Job();
+    Job job2 = new Job();
+    
+    job1._jobTitle = "Vampire";
+    job1._company = "Scholomance";
+    job1._startYear = 1925;
+    job1._endYear = 2025;
+    
+    job2._jobTitle = "Ghoul";
+    job2._company = "Scholomance";
+    job2._startYear = 1915;
+    job2._endYear = 2015;
+
+    Resume myResume = new Resume();
+    myResume._name = "Austin";
+    myResume._jobs.Add(job1);
+    myResume._jobs.Add(job2);
+    myResume.DisplayResume();
+        }
 }
