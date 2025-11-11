42295d4bdfe96496c778df633a381a7d9fcee5a2
johnvak01 <johnvak01@gmail.com>
2025-11-10 17:43:37 -0700

    completed fraction assignment for week 03

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        
+        Fraction tester1 = new Fraction();
+        Fraction tester2 = new Fraction(5);
+        Fraction tester3 = new Fraction(2,3);
+
+        Console.WriteLine(tester1.GetFractionString());
+        Console.WriteLine(tester2.GetFractionString());
+        Console.WriteLine(tester3.GetFractionString());
+        
+        Console.WriteLine(tester1.GetDecimalValue());
+        Console.WriteLine(tester2.GetDecimalValue());
+        Console.WriteLine(tester3.GetDecimalValue());
     }
 }
