2593fd1124b1f341c5df4f569885f94d734a7e0c
johnvak01 <johnvak01@gmail.com>
2025-11-20 18:51:38 -0700

    updated for better text alignment

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,14 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
         // make Products (based on cards from Magic the Gathering)
         Product blackLotus      = new Product("Black Lotus",        1 , 1, 3.01);
         Product oneRing         = new Product("The One Ring",       2 , 2, 2.64);
         Product copyArtifact    = new Product("Copy Artifact",      3,  3, 0.10);
         Product timeTwister     = new Product("Time Twister",       4,  4, 0.08);
         Product lordOfPit       = new Product("Lord of Pit",    5,  5, 0.11);
-        Product splendidGenesis = new Product("Splendid Genesis",   6,  6, 0.07);
         // make addresses
         Address whiteHouse = new Address("1600 Pennsylvania Avenue", "Washington DC", "District of Columbia", "USA");
         Address templeSquare = new Address("50 W North Temple Street", "Salt Lake City", "UT", "USA");
@@ -20,7 +18,7 @@ class Program
         Customer russel = new Customer("Russel N.",templeSquare);
         //make orders
         Order christmasOrder = new Order([blackLotus,oneRing,copyArtifact],donald);
-        Order birthdayOrder = new Order([timeTwister,lordOfPit, splendidGenesis],russel);
+        Order birthdayOrder = new Order([timeTwister,lordOfPit, copyArtifact],russel);
 
         // create outputs
         Console.WriteLine(christmasOrder.PackingLabel() + "\n" + "Total:\t$" + christmasOrder.calcTotalCost() + "\n" + christmasOrder.ShippingLabel() + "\n");
